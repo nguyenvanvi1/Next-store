b41cff1cda377745ed7c541f37b6c4778bdfdf7b
nguyenvanvi1 <nvvi.nvla1.2019@gmail.com>
2025-04-06 22:41:13 +0700

    add auth

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,3 +1,5 @@
+'use server';
+import { auth, currentUser } from '@clerk/nextjs/server';
 import db from '@/utils/db';
 import { redirect } from 'next/navigation';
 export const fetchFeaturedProducts = async () => {
@@ -7,6 +9,50 @@ export const fetchFeaturedProducts = async () => {
     },
   });
   return products;
+};
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : 'An error occurred',
+  };
+};
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error('You must be logged in to access this route');
+  }
+  return user;
+};
+export const createProductAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+
+  try {
+    const name = formData.get('name') as string;
+    const company = formData.get('company') as string;
+    const price = Number(formData.get('price') as string);
+    const image = formData.get('image') as File;
+    const description = formData.get('description') as string;
+    const featured = Boolean(formData.get('featured') as string);
+
+    await db.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image: '/images/product-1.jpg',
+        description,
+        featured,
+        clerkId: user.id,
+      },
+    });
+    return { message: 'product created' };
+  } catch (error) {
+    return renderError(error);
+  }
 };
 export const fetchSingleProduct = async (productId: string) => {
   const product = await db.product.findUnique({
